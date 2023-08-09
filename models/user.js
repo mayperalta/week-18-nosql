146009d43ee931ce6051680887f7a9a84ab103bd
May Peralta <mayperalta@MBSs-MBP.lan>
2023-08-09 00:26:49 -0400

    added models

---
+++ models/user.js
@@ -0,0 +1,44 @@
+const { User, model } = require('mongoose');
+
+const userSchema = new User(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/, "Enter a valid email address"]
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'thought',
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'user',
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
